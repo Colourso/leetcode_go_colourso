ca2f1d5af3c90691bc1178f546015747a05c6d1b
Colourso <colourso@outlook.com>
2023-01-01 23:13:22 +0800

    two_sum

---
+++ l1_two_sum/two_sum.go
@@ -0,0 +1,15 @@
+package l1_two_sum
+
+// TwoSum
+// https://leetcode.cn/problems/two-sum/
+// 使用哈希表来做处理
+func TwoSum(nums []int, target int) []int {
+	numMap := make(map[int]int)
+	for index, n := range nums {
+		if key1, ok := numMap[target - n]; ok {
+			return []int{key1, index}
+		}
+		numMap[n] = index
+	}
+	return nil
+}
---
+++ l1_two_sum/two_sum_test.go
@@ -0,0 +1,14 @@
+package l1_two_sum
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestTwoSum(t *testing.T) {
+	// nums = [3,3], target = 6 输出：[0,1]
+	ans1, ans2 := []int{0,1}, []int{1,0}
+	if ans := TwoSum([]int{3,3}, 6); !reflect.DeepEqual(ans, ans1) && !reflect.DeepEqual(ans, ans2) {
+		t.Errorf("nums = [3,3], target = 6 输出：[0,1], but error")
+	}
+}
