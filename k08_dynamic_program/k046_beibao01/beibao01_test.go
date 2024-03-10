db3abb6776f98116f749ee7000e2b4e378d8b70d
colourso <colourso@outlook.com>
2024-03-10 21:18:21 +0800

    01背包

---
+++ k08_dynamic_program/k046_beibao01/beibao01_test.go
@@ -0,0 +1,7 @@
+package k046_beibao01
+
+import "testing"
+
+func TestBeibao(t *testing.T) {
+	Beibao01()
+}
--- main.go
+++ main.go
@@ -19,4 +19,57 @@ func main() {
 
 	dp[1][0] = 1
 	fmt.Println(dp)
+
+	beibao01 := Beibao01()
+	fmt.Println(beibao01)
+}
+
+func Beibao01() int {
+	M, N := 0, 0
+	fmt.Scanln(&M, &N)
+	weight := make([]int, M)
+	value := make([]int, M)
+	for i := 0; i < M; i++ {
+		fmt.Scan(&weight[i])
+	}
+	for i := 0; i < M; i++ {
+		fmt.Scan(&value[i])
+	}
+
+	fmt.Println(M)
+	fmt.Println(N)
+	fmt.Println(weight)
+	fmt.Println(value)
+
+	dp := make([][]int, M)
+	for i := range dp {
+		dp[i] = make([]int, N+1)
+	}
+	for i := 0; i <= N; i++ {
+		if weight[0] <= i {
+			dp[0][i] = value[0]
+		}
+	}
+	fmt.Println(dp)
+
+	// 先物品、后背包
+	for i := 1; i < M; i++ {
+		for j := 1; j <= N; j++ {
+			if weight[i] > j {
+				dp[i][j] = dp[i-1][j]
+			} else {
+				dp[i][j] = getMax(dp[i-1][j], dp[i-1][j-weight[i]]+value[i])
+			}
+		}
+		fmt.Println(dp)
+	}
+
+	return dp[M-1][N]
+}
+
+func getMax(a, b int) int {
+	if a >= b {
+		return a
+	}
+	return b
 }
